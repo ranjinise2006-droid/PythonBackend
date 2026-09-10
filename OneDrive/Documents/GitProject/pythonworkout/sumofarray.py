4b88b09e581fbe2e129e8040ea8c1b62d4cf4625
Ranjini <ranjinise2006@gmail.com>
2026-09-10 16:31:57 +0530

    add the root sum of array script

---
+++ OneDrive/Documents/GitProject/pythonworkout/sumofarray.py
@@ -0,0 +1,12 @@
+'''
+num = [5, 10, 150, 20]
+sum = 0
+for i in num:
+    sum += i
+print("The sum of the array is:", sum)
+'''
+
+#  other way to calculate sum of array
+num = [5, 10, 150, 20]
+sum = sum(num)
+print("The sum of the array is:", sum)
