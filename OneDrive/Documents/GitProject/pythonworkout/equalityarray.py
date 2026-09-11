97d2c8fbd86399c4f1f5d4636a06fc9b90e5f61a
Ranjini <ranjinise2006@gmail.com>
2026-09-11 17:26:53 +0530

    add root equality array script

---
+++ OneDrive/Documents/GitProject/pythonworkout/equalityarray.py
@@ -0,0 +1,16 @@
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [1, 2, 3, 9, 5]
+
+if len(arr1) != len(arr2):
+    are_equal = False
+else:
+    are_equal = True
+    for index in range(len(arr1)):
+        if arr1[index] != arr2[index]:
+            are_equal = False
+            break
+
+if are_equal:
+    print("the arrays are equal")
+else:
+    print("the arrays are not equal")
